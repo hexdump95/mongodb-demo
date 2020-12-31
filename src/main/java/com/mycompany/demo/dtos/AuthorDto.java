bd37d531a24b199af883c8ca0048cbdcbdfc235e
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2020-12-30 23:57:43 -0300

    Add AuthorDto.java

---
+++ src/main/java/com/mycompany/demo/dtos/AuthorDto.java
@@ -0,0 +1,33 @@
+package com.mycompany.demo.dtos;
+
+import java.io.Serializable;
+
+public class AuthorDto implements Serializable {
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public AuthorDto() {
+	}
+
+	public AuthorDto(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
