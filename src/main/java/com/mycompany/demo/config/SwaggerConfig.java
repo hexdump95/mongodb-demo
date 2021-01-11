d1698bbc843b5b7de1b50b420724408c693ce702
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2021-01-11 17:27:01 -0300

    Revert AuthorController

--- src/main/java/com/mycompany/demo/config/SwaggerConfig.java
+++ src/main/java/com/mycompany/demo/config/SwaggerConfig.java
@@ -11,11 +11,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
 
-  @Bean
-  public Docket apiDocket() {
-    return new Docket(DocumentationType.SWAGGER_2).select()
-      .apis(RequestHandlerSelectors.basePackage("com.mycompany.demo.controllers"))
-      .paths(PathSelectors.any()).build();
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.mycompany.demo.controllers")).paths(PathSelectors.any())
+				.build();
 	}
 
 }
---
+++ src/main/java/com/mycompany/demo/controllers/AuthorController.java
@@ -0,0 +1,56 @@
+
+package com.mycompany.demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.demo.dtos.AuthorDto;
+import com.mycompany.demo.services.AuthorService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/authors")
+@CrossOrigin(origins = "*")
+public class AuthorController {
+
+	Logger logger = LoggerFactory.getLogger(AuthorController.class);
+
+	@Autowired
+	private AuthorService service;
+
+	@GetMapping(path = "")
+	public ResponseEntity<Flux<AuthorDto>> getAuthors() {
+		return ResponseEntity.ok(service.findAll());
+	}
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Mono<AuthorDto>> getAuthor(@PathVariable("id") String id) {
+		return ResponseEntity.ok(service.findOne(id));
+	}
+
+//	@PostMapping(path = "")
+//	public ResponseEntity<Mono<AuthorDto>> postAuthor(@RequestBody AuthorDto authorDto) {
+//		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(authorDto));
+//
+//	}
+//
+//	@PutMapping(path = "/{id}")
+//	public ResponseEntity<Mono<AuthorDto>> updateAuthor(@PathVariable String id, @RequestBody AuthorDto authorDto) {
+//		return ResponseEntity.status(HttpStatus.OK).body(service.update(id, authorDto));
+//	}
+//
+//	@DeleteMapping(path = "/{id}")
+//	public ResponseEntity<Mono<String>> deleteAuthor(@PathVariable String id) {
+//		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.delete(id));
+//	}
+
+}
