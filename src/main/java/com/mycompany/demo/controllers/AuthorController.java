f11ad2103e5ef9f7ba599c99b2d8af059c23dfa8
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2021-01-16 14:55:49 -0300

    Add validation to postAuthor

--- src/main/java/com/mycompany/demo/controllers/AuthorController.java
+++ src/main/java/com/mycompany/demo/controllers/AuthorController.java
@@ -1,13 +1,19 @@
-
 package com.mycompany.demo.controllers;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,21 +42,21 @@ public class AuthorController {
 	public ResponseEntity<Mono<AuthorDto>> getAuthor(@PathVariable("id") String id) {
 		return ResponseEntity.ok(service.findOne(id));
 	}
+	
+	@PostMapping(path = "")
+	public ResponseEntity<Mono<AuthorDto>> postAuthor(@Valid @RequestBody AuthorDto authorDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(authorDto));
 
-//	@PostMapping(path = "")
-//	public ResponseEntity<Mono<AuthorDto>> postAuthor(@RequestBody AuthorDto authorDto) {
-//		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(authorDto));
-//
-//	}
-//
-//	@PutMapping(path = "/{id}")
-//	public ResponseEntity<Mono<AuthorDto>> updateAuthor(@PathVariable String id, @RequestBody AuthorDto authorDto) {
-//		return ResponseEntity.status(HttpStatus.OK).body(service.update(id, authorDto));
-//	}
-//
-//	@DeleteMapping(path = "/{id}")
-//	public ResponseEntity<Mono<String>> deleteAuthor(@PathVariable String id) {
-//		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.delete(id));
-//	}
+	}
+
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<Mono<AuthorDto>> updateAuthor(@PathVariable String id, @RequestBody AuthorDto authorDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(service.update(id, authorDto));
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Mono<String>> deleteAuthor(@PathVariable String id) {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.delete(id));
+	}
 
 }
